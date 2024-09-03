f8efd9ab80c7bbf667790c9ee5ec19c9a38cdf64
swornimamishra <swornimamishra@gmail.com>
2024-09-03 12:31:44 +0530

    cors imported

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import AppRouter from './src/routes/index.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 const PORT = process.env.PORT ;
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(AppRouter)
